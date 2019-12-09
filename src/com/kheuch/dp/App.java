7b4de2da148bc24a4eda0a27027f5b975cae91b7
Cheikhou Seck <kheus1990@gmail.com>
2019-12-09 10:04:36 +0100

    import de mes fichiers

---
+++ src/com/kheuch/dp/App.java
@@ -0,0 +1,10 @@
+package com.kheuch.dp;
+
+public class App {
+
+	public static void main(String[] args) {
+		Context c = new Context();
+		c.process();
+	}
+
+}
---
+++ src/com/kheuch/dp/Context.java
@@ -0,0 +1,10 @@
+package com.kheuch.dp;
+
+public class Context {
+	
+	public void process() {
+		
+		System.out.println("je suis la");
+	}
+
+}
